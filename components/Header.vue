7b180d9f0d3ef7b3de484b2fca3bd662767b6ba7
stst5821 <stst5821@gmail.com>
2021-09-16 13:14:07 +0900

    header,index追加

--- components/Header.vue
+++ components/Header.vue
@@ -1,16 +1,15 @@
 <template>
-<div>
- <nav class="flex">
-   <img class="w-10" src="~@/assets/images/Header/title_icon.svg" alt="">
-  <ul class="flex">
-    <li><a href="#">Home</a></li>
-    <li><a href="#">Art</a></li>
-    <li><a href="#">Commissions</a></li>
-    <li><a href="#">Studio</a></li>
-    <li><a href="#">Journal</a></li>
-  </ul>
-</nav>
-
+<div class="w-10/12 mx-auto mt-32 mb-20">
+ <nav class="flex justify-between items-center">
+   <img class="w-14" src="~@/assets/images/Header/title_icon.svg" alt="">
+    <ul class="flex mr-28">
+      <li class="Header__list"><a class="Header__link" href="#">Home</a></li>
+      <li class="Header__list"><a class="Header__link" href="#">Art</a></li>
+      <li class="Header__list"><a class="Header__link" href="#">Commissions</a></li>
+      <li class="Header__list"><a class="Header__link" href="#">Studio</a></li>
+      <li class="Header__list"><a class="Header__link" href="#">Journal</a></li>
+    </ul>
+  </nav>
 </div>
 </template>
 
@@ -20,4 +19,10 @@
 
 
 <style>
+.Header__list {
+  margin: 0 1.5rem;
+}
+.Header__link {
+  font-size: 18px;
+}
 </style>
