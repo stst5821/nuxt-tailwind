cb414aa25e0e62564ea48d0c57447bfa9deab749
stst5821 <stst5821@gmail.com>
2021-09-17 15:22:28 +0900

    package.json修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,5 +44,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+  router: {
+    base: '/nuxt-tailwind/'
+  },
+  generate: {
+    dir: 'docs'
+  },
 }
