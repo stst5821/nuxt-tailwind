9c11a6facda096ba490b974113f499b24532a99f
stst5821 <stst5821@gmail.com>
2021-09-17 14:18:16 +0900

    studio追加

---
+++ pages/Studio.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="flex pt-14">
+    <div class="Studio__wrap ml-64">
+      <div class="mb-32">
+        <p class="Studio__text-large mb-32">Pierrick Calvez, Art & Illustration, Design & Visual Identities.</p>
+        <p class="Studio__text-mid">Business or exhibition inquiries, please send an email to studio (at) pierrickcalvez (dot) com.</p>
+      </div>
+
+      <div class="Studio__public mb-12">
+        <p class="Studio__title">Publications</p>
+        <p class="Studio__subtitle mb-5">BOOKS</p>
+        <div class="Studio__text-small">
+          <p><a class="Studio__link hover:text-red-500" href="#">トフォンサイトデザインのアイデア帳  ed. Impress <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">The Smashing Idea Book ed. Smashing Mag <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">Best 365 Websites Around the World ed. Awwwards <span class="text-red-500">></span></a></p>
+          <p>Web Design ed. Oracom</p>
+          <p>Design in Europe ed. Pyramyd</p>
+          <p>Websites ed. Page One Publishing</p>
+          <p>Web Design Index ed. Agile Rabbit</p>
+          <p>Worldwide Designers ed. Populaire</p>
+          <p>1000 Favourite Websites ed. Taschen</p>
+        </div>
+      </div>
+
+      <div class="Studio__public mb-12">
+        <p class="Studio__subtitle mb-5">ONLINE</p> 
+        <div class="Studio__text-small">
+          <p><a class="Studio__link hover:text-red-500" href="#">Press Matter Interview <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">People of Print Interview <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">Department Store Showcase <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">Spotlight Magazine Interview <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">Spotlight Magazine Interview <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">Siteinspire Showcase <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">Awwwards Showcase <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">Muzli Showcase <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">Digital Abstracts Showcase <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">Shunrize Interview <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">Webby's Showcase <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">FWA Interview <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">FWA Index Showcase <span class="text-red-500">></span></a></p>
+          <p>OFFF Showcase</p>
+          <p><a class="Studio__link hover:text-red-500" href="#">Arts & Littératures Numériques Showcase <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">La Ville Numérique Showcase <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">Davidseah Article <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">Pinterest Showcase <span class="text-red-500">></span></a></p>
+        </div>
+      </div>
+
+      <div class="Studio__public mb-12">
+        <p class="Studio__subtitle mb-5">PRESS</p>
+        <div class="Studio__text-small">
+          <p><a class="Studio__link hover:text-red-500" href="#">Streem Magazine Interview <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">Computer Arts Interview <span class="text-red-500">></span></a></p>
+          <p>Étapes Article</p>
+          <p>Libération Article</p>
+          <p>Paris-Art Article</p>
+          <p>L'internaute Article</p>
+          <p>Telerama Article</p>
+        </div>
+      </div>
+
+    </div>
+
+  <div class="Studio__wrap ml-32">
+    <div class="text-gray-500 mb-32">
+      <p class="mb-5">Pierrick Calvez<br>
+          Wild Island<br>
+          Nº39 HuangJiaTang Street<br>
+          Huicheng District, Huizhou City<br>
+      </p>
+      <p class="mb-5">
+        studio@pierrickcalvez.com<br>
+        Phone: +86 136 8085 1390<br>
+        Wechat @pierrickcalvez<br>
+      </p>
+      <p class="mb-5">
+        Ello<br>
+        Medium<br>
+        Instagram<br>
+      </p>
+    </div>
+
+      <div class="Studio__public mb-12">
+        <p class="Studio__title">Public Events</p>
+        <p class="Studio__subtitle mb-5">EXHIBITIONS</p>
+        <div class="Studio__text-small">
+          <p>The Land Below Huizhou</p>
+          <p>Singularity Shenzhen</p>
+          <p>In the Wan Light of Carina Huizhou</p>
+          <p>Belvedere Festival Rome</p>
+          <p>Seoul International Film Festival Seoul</p>
+          <p>Poésie à la BNF Paris</p>
+          <p><a class="Studio__link hover:text-red-500" href="#">Japan Media Arts Festival Tokyo <span class="text-red-500">></span></a></p>
+          <p>File Festival São Paulo</p>
+          <p>Le Cube Paris</p>
+          <p><a class="Studio__link hover:text-red-500" href="#">Les Rendez-Vous Electroniques Paris <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">La Ville Numérique Showcase <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">Flash Festival Paris <span class="text-red-500">></span></a></p>
+          <p>BD4D Paris</p>
+        </div>
+      </div>
+
+      <div class="Studio__public mb-12">
+        <p class="Studio__subtitle mb-5">TALKS</p>
+        <div class="Studio__text-small">
+          <p><a class="Studio__link hover:text-red-500" href="#">PechaKucha Shanghai <span class="text-red-500">></span></a></p>
+          <p><a class="Studio__link hover:text-red-500" href="#">UI/UX Conferences Shanghai <span class="text-red-500">></span></a></p>
+          <p>Le Club des DA Paris</p>
+          <p>PechaKucha Paris</p>
+          <p>Fête de l'Internet Le Havre</p>
+          <p>Nemo Forum des Images Paris</p>
+          <p>Histoires Interactives Caen</p>
+          <p>Histoires Interactives Montpellier</p>
+        </div>
+      </div>
+      
+       <div class="Studio__public mb-12">
+        <p class="Studio__title mb-2">Clients</p>
+        <div class="text-gray-500 Studio__text-small">
+          <p>Air France</p>
+          <p>Airbus</p>
+          <p>BMW</p>
+          <p>Chanel</p>
+          <p>Chateau Miraval</p>
+          <p>Chloe</p>
+          <p>Citroen</p>
+          <p>Denza</p>
+          <p>Diptyque</p>
+          <p>Disney</p>
+          <p>Ducasse</p>
+          <p>Ideo</p>
+          <p>Krug</p>
+          <p>Lacoste</p>
+          <p>Le Coq Sportif</p>
+          <p>Loewe</p>
+          <p>Lufthansa</p>
+          <p>LV Foundation</p>
+          <p>Maje</p>
+          <p>Nespresso</p>
+          <p>Nike</p>
+          <p>Nina Ricci</p>
+          <p>Perrier-Jouët</p>
+        </div>
+      </div>
+
+      <div class="Studio__copyright text-gray-500">
+        <p>Website & Artworks Copyright Pierrick Calvez</p>
+        <p>Fonts in use: Apercu, Domaine Display</p>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<style>
+.Studio__wrap {
+  max-width: 360px;
+}
+.Studio__public {
+  width: 380px;
+}
+.Studio__title {
+  font-size: 1.563rem;
+}
+.Studio__subtitle {
+  font-size: 0.938rem;
+}
+.Studio__text-large {
+  font-size: 2.875rem;
+  line-height: 3.25rem;
+}
+.Studio__text-mid {
+  font-size: 1.375rem;
+  line-height: 1.875rem;
+}
+.Studio__text-small {
+  font-size: 0.938rem;
+}
+.Studio__copyright {
+  font-size: 0.688rem;
+}
+</style>
